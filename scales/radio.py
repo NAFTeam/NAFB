0460e8d2cada81e39909198ba3d84fa25f174e1a
LordOfPolls <ddavidallen13@gmail.com>
2022-10-19 12:18:52 +0100

    feat: update to latest naff link

--- scales/radio.py
+++ scales/radio.py
@@ -2,46 +2,53 @@ import asyncio
 
 from naff import Extension, listen, GuildVoice
 from naff.api.events import VoiceStateUpdate
-from naff_link.client import Client as LinkClient
+from naff_link import NaffLink
 
 
 class Radio(Extension):
+    def __init__(self, _):
+        self.bot.load_extension("naff_link")
+        self.naff_link: NaffLink = self.bot.naff_link
+
     @listen()
     async def on_startup(self):
-        self.naff_link = await LinkClient.initialize(self.bot)
-        await self.naff_link.connect_to("lavalink", 2333, "youshallnotpass")
+        self.naff_link.add_node("localhost", 2333, "youshallnotpass", "eu")
 
     @listen()
     async def on_voice_state_update(self, event: VoiceStateUpdate):
+
         channel_id = event.before.channel.id if event.before else event.after.channel.id
-        channel = self.bot.get_channel(channel_id)
+        latest_channel = self.bot.get_channel(channel_id)
         member = event.before.member if event.before else event.after.member
 
-        if member.id != self.bot.user.id:
-            if channel.name == "lofi-radio":
-                vc = self.bot.get_bot_voice_state(channel.guild.id)
-                if not vc and event.after is not None:
-                    return await self.start_radio(channel)
-                else:
-                    asyncio.create_task(self.should_leave(channel))
+        if member.id == self.bot.user.id:
+            return
 
-    @listen()
-    async def on_stats_update(self, event):
-        self.latest_stats = event.stats
-
-    @listen()
-    async def on_player_update(self, event):
-        self.player_state = event.state
+        if latest_channel.name == "lofi-radio":
+            vc_player = self.naff_link.get_player(latest_channel.guild)
+            if (not vc_player or not vc_player.is_connected) and event.after:
+                return await self.start_radio(latest_channel)
+            else:
+                asyncio.create_task(self.should_leave(latest_channel))
 
     async def start_radio(self, channel: GuildVoice):
-        vc = await self.naff_link.voice_connect(channel, channel.guild)
-        await vc.play("https://www.youtube.com/watch?v=jfKfPfyJRdk")
+        await self.naff_link.ready.wait()
+        vc_player = await self.naff_link.connect_to_vc(channel.guild, channel)
+
+        results = await self.naff_link.get_tracks(
+            "https://www.youtube.com/watch?v=jfKfPfyJRdk"
+        )
+        await vc_player.play(results.tracks[0])
 
     async def should_leave(self, channel: GuildVoice):
         await asyncio.sleep(5)
         if len(channel.voice_members) == 1:
-            if vc := self.bot.get_bot_voice_state(channel.guild.id):
-                await vc.disconnect()
+            if channel.voice_members[0].id == self.bot.user.id:
+                await self.naff_link.disconnect(channel.guild)
+
+    @listen()
+    async def on_queue_end_event(self, event):
+        await self.start_radio(event.player.channel)
 
 
 def setup(bot):
